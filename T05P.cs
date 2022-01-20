4b6965ece5de7ec7df8c624b8f80557c652d3635
Álvaro Matías Duré <72114981+alvarodure@users.noreply.github.com>
2022-01-20 16:14:32 -0300

    Variables e Inspeccion
    Starting with c# sintax

---
+++ T05P.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//VARIABLES E INSPECCION
+public class T05P : MonoBehaviour
+{
+    void Start()
+    {
+        // INT FLOAT SRTING BOOL
+     int añoamd = 22;
+     float notamate = 76.5f;
+     string nombreamd = "Alvaro";
+     bool lifeamd=true;
+     double dou = 1.345;
+     Debug.Log(añoamd);
+     Debug.Log(notamate);
+     Debug.Log(nombreamd);
+     Debug.Log(lifeamd);
+     Debug.Log(dou);
+    //  Debug.Log(añoamd,notamate,nombreamd,lifeamdañoamd,notamate,nombreamd,lifeamd,dou,dou);
+    //  Print(añoamd,notamate,nombreamd,lifeamd,dou);
+    }
+}
